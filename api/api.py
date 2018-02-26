c232a56d03b53f1a0a7552f1edb6c8d61be24de7
txstu09 <sgillies09@gmail.com>
2018-02-26 17:16:43 -0600

    add waffle route

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
 #----------------------------
 # IMPORTS
 #----------------------------
-from flask import Flask, request
+from flask import Flask, request, jsonify
+
+import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -28,3 +30,33 @@ inspector = inspect(engine)
 #----------------------------
 # BUILD FLASK ROUTES
 #----------------------------
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Main page goes here"
+
+@app.route('/recruits/starcount/')
+def starcount():
+    '''
+    Returns dict with two lists.
+        STARS: Recruit star ratings
+        AVG_YEAR: Average count of recruits for each star rating
+    '''
+
+    df = pd.read_sql_table('cfb_recruits', engine)
+    df = df.groupby(['STAR']).count().reset_index()
+    df['STAR'] = ['One','Two','Three','Four','Five']
+    df['PLAYER'] = df['PLAYER'].apply(lambda x: int(round(x*0.17)))
+    df = df[['STAR','PLAYER']]
+    df = df.rename(columns={'STAR':'STARS','PLAYER':'AVG_YEAR'})
+
+    star_dict = df.to_dict('list')
+
+    return jsonify(star_dict)
+
+@app.route()
+
+if __name__ == '__main__':
+    #app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
